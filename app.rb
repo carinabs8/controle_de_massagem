cf76f1e1c57a259bffb5c60b57d649c9322f7f1a
Carina Brito <carinabs8@gmail.com>
2016-03-23 01:22:20 -0300

    Configuracao do puma na aplicacao.

--- app.rb
+++ app.rb
@@ -2,6 +2,21 @@ require 'sinatra'
 require 'sinatra/activerecord'
 require './config/environments'
 
+require 'rubygems'
+require 'bundler'
 
-class Usuario < ActiveRecord::Base
+Bundler.require
+
+module ControleDeMassagem
+	class App < Sinatra::Application
+		module Models
+	    autoload :Usuario, 'app/models/usurio'
+	  end
+		# use Routes::Usuarios
+		set :sessions,
+	    :httponly     => true,
+	    :secure       => production?,
+	    :expire_after => 31557600, # 1 year
+	    :secret       => ENV['SESSION_SECRET']
+	end
 end
